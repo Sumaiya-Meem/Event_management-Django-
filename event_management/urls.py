920770c7b5205edb20a30f875feb84c2f96c6fcf
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2025-02-25 20:56:03 +0600

    make this project responsive

--- event_management/urls.py
+++ event_management/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.http import HttpResponse
+from debug_toolbar.toolbar import debug_toolbar_urls
 # from tasks.views import
 
 def home_view(request):
@@ -11,4 +12,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
      path('', home_view, name='home'),
      path('events/', include("events.urls")),
-]
+]+ debug_toolbar_urls()
