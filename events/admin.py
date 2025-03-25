e272331b391d4bd5e0edaf54d6e2446cd3a9ff46
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2025-03-25 22:27:40 +0600

    add rsvp model in admin site

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from events.models import Event,Category
+from events.models import Event,Category,RSVP
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Category)
+admin.site.register(RSVP)
 # admin.site.register(Participant)
